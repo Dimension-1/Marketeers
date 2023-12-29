0eed5c3bad421fbfd23d359947b664a3f72ad243
Srishti Lodhi <srishtilodhi38@gmail.com>
2023-12-30 02:14:25 +0530

    blog updates

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import blog from "../assets/blog.png";
 import arrow from "../assets/arrow-review.svg";
 import blog2 from "../assets/blog-image2.png"
@@ -6,32 +6,34 @@ import blog3 from "../assets/blog3.png";
 import { Link } from "react-router-dom";
 
 const Blogs = () => {
+  const [isSuccess, setIsSuccess] = useState(false);
 
-  // function Submit(e) {
-  //   e.preventDefault();
-  //   const formEle = document.querySelector("form");
-  // console.log("Form Values:", {
-  //   subscribe: formEle.querySelector("[name='subscribe']").value,
-  // });
-  // const formDatab = new FormData(formEle);
-  // console.log("FormData Object:", formDatab);
+  function Submit(e) {
+    e.preventDefault();
+    const formEle = document.querySelector("form");
+  console.log("Form Values:", {
+    subscribe: formEle.querySelector("[name='subscribe']").value,
+  });
+  const formDatab = new FormData(formEle);
+  console.log("FormData Object:", formDatab);
   
   
-  //   fetch(
-  //     "https://sheet.best/api/sheets/dad9f654-6401-4499-aff6-779c7c9d9c59",
-  //     {
-  //       method: "POST",
-  //       body: formDatab
-  //     }
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }
+    fetch(
+      "https://sheet.best/api/sheets/dad9f654-6401-4499-aff6-779c7c9d9c59",
+      {
+        method: "POST",
+        body: formDatab
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setIsSuccess(true);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   const blogData = [
     {
@@ -169,62 +171,64 @@ const Blogs = () => {
         </div>
       </div>
 
-      <form className="form" >
-      <div
-        className="subscribe d-lg-flex d-md-flex pt-3 mt-md-5 subscriptions"
-        style={{ paddingLeft: "5rem", paddingRight: "5rem" }}
-      >
-       
-        <div className="col-md-6 col-7 d-flex flex-column justify-content-between ">
-          <h2
-            className="w-75 subscription"
+      <form className="form" onSubmit={(e) => Submit(e)}>
+
+        
+      
+  {isSuccess ? (
+    <div className="subscribe d-lg-flex d-md-flex justify-content-center align-items-center mt-md-5 subscriptions" style={{ padding: "2rem" }}>
+    <div className="success" style={{
+            color: "var(--primary-text)",
+            fontSize: "1.3rem",
+            fontWeight: "500",
+            opacity: "0.8",
+          }}>Thank you! You have joined our newsletter!</div>
+    </div>
+  ) : (
+    
+    <div className="subscribe d-lg-flex d-md-flex pt-3 mt-md-5 subscriptions" style={{ paddingLeft: "5rem", paddingRight: "5rem" }}>
+      <div className="col-md-6 col-7 d-flex flex-column justify-content-between">
+        <h2 className="w-75 subscription" style={{ fontSize: "2.5rem", color: "var(--primary-text)", fontWeight: "500" }}>
+          Don't want to miss anything?
+        </h2>
+        <div>
+          <input
             style={{
-              fontSize: "2.5rem",
-              color: "var( --primary-text)",
+              border: "none",
+              borderBottom: "0.1px solid var(--line-color)",
+              outline: "none",
+              backgroundColor: "transparent",
+              color: "var(--text-color)",
+              fontSize: "1.5rem",
               fontWeight: "500",
             }}
-          >
-            Don't want to miss anything?
-          </h2>
-          <div>
-            <input
-              style={{
-                border: "none",
-                borderBottom: "0.1px solid var(--line-color)", // You can set the color of the line here
-                outline: "none", // Remove the default input focus outline
-                backgroundColor: "transparent",
-                color: "var(--text-color)",
-                fontSize: "1.5rem",
-                fontWeight: "500",
-              }}
-              type="text"
-                  id="subscribe"
+            type="text"
+            id="subscribe"
             name="subscribe"
-              placeholder="Email"
-            />
-            <div
-              className="green-border d-flex justify-content-center pt-4 w-75 mt-2"
-              style={{ borderColor: "var(--text-color)" }}
-            ></div>
-          </div>
+            placeholder="Email"
+          />
+          <div
+            className="green-border d-flex justify-content-center pt-4 w-75 mt-2"
+            style={{ borderColor: "var(--text-color)" }}
+          ></div>
         </div>
-        <div className="col-md-6 col-5 d-flex flex-column justify-content-between ">
-          <p
-            className="w-75"
-            style={{
-              color: "var(--primary-text)",
-              fontSize: "1.3rem",
-              fontWeight: "500",
-              opacity: "0.8",
-            }}
-          >
-            Sign up for our newsletter to discover winning trends before your
-            competition!
-          </p>
-          <div>
-            <div className="d-flex justify-content-between w-75"> 
+      </div>
+      <div className="col-md-6 col-5 d-flex flex-column justify-content-between">
+        <p
+          className="w-75"
+          style={{
+            color: "var(--primary-text)",
+            fontSize: "1.3rem",
+            fontWeight: "500",
+            opacity: "0.8",
+          }}
+        >
+          Sign up for our newsletter to discover winning trends before your competition!
+        </p>
+        <div>
+          <div className="d-flex justify-content-between w-75">
             <button
-            type="submit"
+              type="submit"
               className="fw-medium"
               style={{
                 backgroundColor: "transparent",
@@ -232,12 +236,11 @@ const Blogs = () => {
                 fontSize: "1.5rem",
               }}
             >
-              
               Subscribe
             </button>
             <span>
-            <img
-                src={arrow} // Replace with the path to your image
+              <img
+                src={arrow}
                 alt="Circle Image"
                 style={{
                   rotate: "180deg",
@@ -245,16 +248,17 @@ const Blogs = () => {
                 }}
               />
             </span>
-            </div>
-            <div
-              className="green-border d-flex justify-content-center pt-4 w-75"
-              style={{ borderColor: "var(--text-color)" }}
-            ></div>
-            
           </div>
+          <div
+            className="green-border d-flex justify-content-center pt-4 w-75"
+            style={{ borderColor: "var(--text-color)" }}
+          ></div>
         </div>
-       
       </div>
+    </div>
+  )}
+
+
       </form>
 
 
